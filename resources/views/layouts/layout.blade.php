a260d4f235a9c8e53faea5b71479eda89a35c0f3
Reginaldo Kambinda <reginaldo.kambinda@digitalfactory.co.ao>
2020-04-06 01:24:43 +0100

    Criação da UI do projeto Montain Stability

---
+++ resources/views/layouts/layout.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Stabilit</title>
+    <link rel="stylesheet" href="{{asset('css/front/bootstrap.css')}}" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="{{asset('css/front/style.css')}}" rel="stylesheet" type="text/css">
+</head>
+<body>
+    <header class="page-header">
+        <div class="container">
+            <div class="d-flex pt-3 align-content-center justify-content-between">
+                <div>
+                    <img src="img/logo.jpg" alt="" width="100">
+                </div>
+                <div>
+                     <h5>PT</h5>
+                </div>
+            </div>
+        </div>
+     </header>
+     <main>
+        @yield('content')
+       
+    </main>
+      <!-- Bootstrap core JavaScript-->
+  <script src="{{ asset('js/front/jquery-3.4.1-slim.min.js') }}"></script>
+  <script src="{{ asset('js/front/bootstrap.bundle.min.js') }}"></script>
+  <script src="{{ asset('js/front/popper.min.js') }}"></script>
+
+</body>
+</html>
